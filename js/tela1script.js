32002ccc1c6ba480e791dadceca1ca4f21bea12f
Arthur Akira <arthurak10.at@gmail.com>
2022-04-21 11:13:06 -0300

    Redireciona para tela 2

--- js/tela1script.js
+++ js/tela1script.js
@@ -1,21 +1,12 @@
+let quizzesObj = [];
+
 function listarQuizzes (response) {
-    console.log(response.data);
+    quizzesObj = response.data;
+    console.log(quizzesObj);
     const quizzes = document.querySelector(".all-quizzes .quizzes");
     for (let i = 0; i < response.data.length; i++) {
         quizzes.innerHTML += `
-        <figure class="quizz-figure">
-            <div class="gradient"></div>   
-            <img class="quizz-image" src="${response.data[i].image}">
-            <h4>${response.data[i].title}</h4>
-        </figure>`;
-        quizzes.innerHTML += `
-        <figure class="quizz-figure">
-            <div class="gradient"></div>   
-            <img class="quizz-image" src="${response.data[i].image}">
-            <h4>${response.data[i].title}</h4>
-        </figure>`;
-        quizzes.innerHTML += `
-        <figure class="quizz-figure">
+        <figure class="quizz-figure" id="${response.data[i].id}" onclick="entrarQuizz(this)">
             <div class="gradient"></div>   
             <img class="quizz-image" src="${response.data[i].image}">
             <h4>${response.data[i].title}</h4>
@@ -26,6 +17,27 @@ function listarQuizzes (response) {
 function buscarQuizzes() {
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes');
     promise.then(listarQuizzes);
+}
+
+function exibirQuizz (quizz) {
+
+    const tela2 = document.querySelector(".tela-2");
+    tela2.innerHTML = `
+    
+
+    `;
+
+
+}
+
+function entrarQuizz (el) {
+    document.querySelector(".container-tela-1").classList.add("hidden");
+    document.querySelector(".tela-2").classList.remove("hidden");
+    const idQuizz = el.getAttribute("id");
+    //console.log(idQuizz);
+
+    const quizz = quizzesObj.filter((quizz) => quizz.id == idQuizz)[0];
+    console.log(quizz);
 }
 
 buscarQuizzes();
